719116eb6a4371137265d0b755bf38d56c23ea7d
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2014-11-26 14:17:40 +0200

    HW7. Andrey Kopitsa

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,5 +8,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 )

5e2649e5181086ab58dde53e728f11abdb6eaff0
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2014-11-26 14:27:21 +0200

    HW7. Andrey Kopitsa

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^bike/$', TemplateView.as_view(template_name='bike.html'), name='bike'),
     url(r'^admin/', include(admin.site.urls)),
 )
